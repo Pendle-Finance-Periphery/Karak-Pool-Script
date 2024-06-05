783313d2d16daa434a8fa3688a6fadc5a025e30a
ngfam <ngfamgithub@gmail.com>
2024-06-05 09:35:27 +0700

    Karak weETH config

--- src/consts.ts
+++ src/consts.ts
@@ -21,29 +21,29 @@ export const CONFIG = {
 
 export const PENDLE_POOL_ADDRESSES = {
     id: 'Karak SUSDE mainnet (September)',
-    SY: '0x1b641894e66aec7bf5ab86517e8d81763cc8e19e',
-    YT: '0xdd68f5d26bf1182d6b241108bfc01f95c030502b',
-    LP: '0xb1f587b354a4a363f5332e88effbbc2e4961250a',
+    SY: '0x8db42af6b2de9e8aee47f3423570d8e9c3873796',
+    YT: '0x5439c3ef0072e4a19c44478cdf947f5d957e66c7',
+    LP: '0x18bafcabf2d5898956ae6ac31543d9657a604165',
     MULTICALL: '0xca11bde05977b3631167028862be2a173976ca11',
-    START_BLOCK: 19967918,
+    START_BLOCK: 19967933,
     TREASURY: '0x8270400d528c34e1596ef367eedec99080a1b592',
-    PENPIE_RECEIPT_TOKEN: '0x1c055d8081ca3b2be0dd00abe784b142c608219e',
-    EQB_STAKING: '0xcb7212d19d52b53e44625dc1a9d4b1f1d4833575',
-    STAKEDAO_RECEIPT_TOKEN: '0xe492c6ba9a458ef0f44581e82d6da885b99d4027',
+    PENPIE_RECEIPT_TOKEN: '0xc683e9a72a2cc87b2110cf680705a21a637fe04e',
+    EQB_STAKING: '0xb55a1785a2edaddad3d373b4ecf5916b9d066733',
+    STAKEDAO_RECEIPT_TOKEN: '0x0b493988b2425218068767f18dd1ecf2f0084c3d',
     LIQUID_LOCKERS: [
       {
         // Penpie
         address: '0x6e799758cee75dae3d84e09d40dc416ecf713652',
-        receiptToken: '0x1c055d8081ca3b2be0dd00abe784b142c608219e',
+        receiptToken: '0xc683e9a72a2cc87b2110cf680705a21a637fe04e',
       },
       {
         // EQB
         address: '0x64627901dadb46ed7f275fd4fc87d086cff1e6e3',
-        receiptToken: '0xcb7212d19d52b53e44625dc1a9d4b1f1d4833575',
+        receiptToken: '0xb55a1785a2edaddad3d373b4ecf5916b9d066733',
       },
       {   // STAKEDAO
           address: '0xd8fa8dc5adec503acc5e026a98f32ca5c1fa289a',
-          receiptToken: '0xe492c6ba9a458ef0f44581e82d6da885b99d4027',
+          receiptToken: '0x0b493988b2425218068767f18dd1ecf2f0084c3d',
       }
     ],
   };
